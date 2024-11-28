c16a49a04e29c48efbb78f4f52cc91b47c6c41e9
Rikard Karlsson <karlsson0214@gmail.com>
2024-11-28 11:25:32 +0100

    listor

---
+++ 7100_listor/exempel_7_slice.py
@@ -0,0 +1,7 @@
+# index     0    1    2    3    4    5    6    7
+my_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']  
+print(my_list[2]) # c
+print(my_list[:2]) # ['a', 'b']
+print(my_list[2:5])  # ['c', 'd', 'e']
+print(my_list[2:]) # ['c', 'd', 'e', 'f', 'g', 'h']
+print(my_list[:2, 6:] )
