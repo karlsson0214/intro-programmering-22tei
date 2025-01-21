339ea5a973fbc1dd6d1bc5fd52add6ee7fa7fbcc
Rikard Karlsson <karlsson0214@gmail.com>
2025-01-21 14:38:27 +0100

    unicode colors

--- 7100_listor/exempel_8_count.py
+++ 7100_listor/exempel_8_count.py
@@ -1,10 +1,29 @@
-hand = [["spader", 2], ["ruter", "kung"],['\u0394', 2], ["ruter", 2], ["klöver", 2]]
+import random
+colors = ['\u2660', '\u2661', '\u2662', '\u2663']
+names = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+name_to_value = {}
+value = 2
+for name in names:
+    name_to_value[name] = value
+    value += 1
 
+deck = []
+for color in colors:
+    for value in names:
+        deck.append([value, color])
+
+
+random.shuffle(deck)
+hand = []
+for i in range(5):
+    hand.append(deck.pop())
+
+print(hand)     
 
 antal_2 = 0
-for kort in hand:
-    print(kort)
-    if kort[1] == 2:
+for kort in deck:
+    # print(kort[0], kort[1])
+    if kort[0] == '2':
         antal_2 += 1
 
 print(f"Antal tvåor: {antal_2}")
